65033f5fa761e7b68e110974f2fb7d7af14d9956
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-07-20 10:08:59 +0200

    stage 2

--- QuizzList.java
+++ QuizzList.java
@@ -20,76 +20,12 @@ public class QuizzList {
     public QuizzList(){
         this.questions = new ArrayList<>();
     }
-//    public QuizzList(){
-//        questions=List.of(
-//                Question.of(1,
-//                "The java logo",
-//                "What is depicted on the Java logo?",
-//                2,"Robot","Tea leaf","Cup of coffee","Bug"),
-//                Question.of(2,
-//                        "The Ultimate Question",
-//                        "What is the answer to the Ultimate Question of Life, the Universe and Everything?",
-//                        2,"Everything goes right","42","2+2=4","11011100")
-//        );
-//    }
+
 
     public List<Question> getAll() {
         return questions;
     }
-//    public  String questionsToJson() throws IOException {
-//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//        JsonFactory factory = new JsonFactory();
-//        JsonGenerator jsonGenerator = factory.createGenerator(stream, JsonEncoding.UTF8);
-//
-//
-//
-//        jsonGenerator.writeStartArray();
-//
-//        questions.stream()
-//                .forEach(question -> {
-//                    try {
-//                        jsonGenerator.writeStartObject();
-//                            jsonGenerator.writeNumberField("id",question.getId());
-//                            jsonGenerator.writeStringField("title",question.getTitle());
-//                            jsonGenerator.writeStringField("text",question.getText());
-//                            jsonGenerator.writeFieldName("options");
-//                                jsonGenerator.writeStartArray();
-//                                for(String option : question.getOptions()){
-//                                    jsonGenerator.writeString(option);
-//                                }
-//                                jsonGenerator.writeEndArray();
-//                        jsonGenerator.writeEndObject();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                });
-//        jsonGenerator.writeEndArray();
-//
-//
-//        return new String(stream.toByteArray(),"UTF-8");
-//
-//    }
 
-//    public String questionToJson(int id) throws IOException {
-//        Question question = getById(id);
-//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//        JsonFactory factory = new JsonFactory();
-//        JsonGenerator generator = factory.createGenerator(stream,JsonEncoding.UTF8);
-//        generator.writeStartObject();
-//        generator.writeNumberField("id",question.getId());
-//        generator.writeStringField("title",question.getTitle());
-//        generator.writeStringField("text",question.getText());
-//        generator.writeFieldName("options");
-//        generator.writeStartArray();
-//        for(String option: question.getOptions()){
-//            generator.writeString(option);
-//        }
-//        generator.writeEndArray();
-//        generator.writeEndObject();
-//
-//        return new String(stream.toByteArray(),"UTF-8");
-//
-//    }
     public Question getById(int id){
         return questions.stream().filter( q -> q.getId() == id)
                 .collect(Collectors.toList()).get(0);
